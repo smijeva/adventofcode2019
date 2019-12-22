3e21aaeb27508a4afe5b2cfa9a0845db450355c8
Eva Šmijáková <evas@kentico.com>
2019-12-22 14:56:34 +0000

    Day 1, part 2 compeleted

--- day1.py
+++ day1.py
@@ -1,13 +1,15 @@
 def mass_to_fuel(mass):
-  mass_int = int(mass)
-  return (mass_int // 3) - 2
+  fuel_for_mass = (mass // 3) - 2
+  if fuel_for_mass < 0:
+    return 0
+  return fuel_for_mass + mass_to_fuel(fuel_for_mass)
 
 
 def main():
   file = open("input1.txt")
   fuel_sum = 0
   for line in file:
-    fuel_sum += mass_to_fuel(line)
+    fuel_sum += mass_to_fuel(int(line))
   print(fuel_sum)
   file.close()
 
@@ -20,10 +22,10 @@ def test():
   assert t2 == 2, f"t2 = {t2}"
 
   t3 = mass_to_fuel(1969)
-  assert t3 == 654, f"t3 = {t3}"
+  assert t3 == 966, f"t3 = {t3}"
 
   t4 = mass_to_fuel(100756)
-  assert t4 == 33583, f"t4 = {t4}"
+  assert t4 == 50346, f"t4 = {t4}"
 
   print("ok")
 
