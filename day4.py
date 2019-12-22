04f45d930dcfd68196655931eba9920f9223b78b
Eva Šmijáková <smijakova.eva@gmail.com>
2019-12-22 21:12:06 +0000

    Day 4, part 2

--- day4.py
+++ day4.py
@@ -4,14 +4,28 @@ range2 = 790572
 def is_valid_pass(number_str):
   last_number = -1
   has_double = False
-  for c in number_str:
-    current = int(c)
+  numbers = list(map(lambda i: int(i), list(number_str)))
+  for i in range(len(numbers)):
+    current = numbers[i]
     if current < last_number:
       return False
     if current == last_number:
-      has_double = True
+      if look_ahead(numbers, i) and look_behind(numbers, i):
+        has_double = True      
     last_number = current
   return has_double
+
+def look_ahead(numbers, i):
+  ahead = i + 1
+  if ahead >= len(numbers):
+    return True
+  return numbers[ahead] != numbers[i]
+
+def look_behind(numbers, i):
+  behind = i - 2
+  if behind < 0:
+    return True
+  return numbers[behind] != numbers[i]
 
 def main():
   valid_count = 0
@@ -22,13 +36,13 @@ def main():
 
 
 def test():
-  t1 = is_valid_pass("111111")
-  assert t1 is True, "111111 is False"
+  t1 = is_valid_pass("112233")
+  assert t1 is True, "112233 is False, True expected"
 
-  t2 = is_valid_pass("223450")
-  assert t2 is False, "223450 is True"
+  t2 = is_valid_pass("123444")
+  assert t2 is False, "123444 is True, False expected"
 
-  t3 = is_valid_pass("123789")
-  assert t3 is False, "123789 is True"
+  t3 = is_valid_pass("111122")
+  assert t3 is True, "111122 is False, True expected"
 
   print("ok")
