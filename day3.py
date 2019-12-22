83da326277715e4c9d562c3e37bd001bc4b4cad3
Eva Šmijáková <smijakova.eva@gmail.com>
2019-12-22 20:43:41 +0000

    Day 3, part 2

--- day3.py
+++ day3.py
@@ -8,11 +8,12 @@ def go(path, direction):
   step_count = int(direction[1:])
   for i in range(step_count):
     last_coor[dir_ix] += dir_step
+    last_coor[2] += 1
     path.append(last_coor.copy())
 
 
 def make_path(directions):
-  path = [ [0, 0] ]
+  path = [ [0, 0, 0] ]
   for d in directions:
     go(path, d)
   return path
@@ -21,14 +22,15 @@ def make_path(directions):
 def intersection(directions1, directions2):
   path1 = make_path(directions1)[1:]
   path2 = make_path(directions2)[1:]
-  hashed_path2 = [str(x) for x in path2]
-  intersections = [value for value in path1 if str(value) in hashed_path2]
-  min_dist = abs(intersections[0][0]) + abs(intersections [0][1])
-  for i in range(len(intersections)):
-    dist = abs(intersections[i][0]) + abs(intersections [i][1])
-    if dist < min_dist:
-      min_dist = dist
-  return min_dist
+  # iterate in diagonals
+  for k in range(len(path2) + len(path1) - 1):
+    for i in range(min(k, len(path1))):
+      j = k - i
+      if j >= len(path2):
+        continue
+      if path1[i][0] == path2[j][0] and path1[i][1] == path2[j][1]:
+        return path1[i][2] + path2[j][2]
+         
 
 def main():
   file = open("input3.txt")
@@ -43,18 +45,18 @@ def main():
 def test():
   t1 = make_path(["U3","R2","D1","L2"])
 
-  assert t1 == [[0,0],[0,1],[0,2],[0,3],[1,3],[2,3],[2,2],[1,2],[0,2]], f"t1 = {t1}"
+  assert t1 == [[0,0,0],[0,1,1],[0,2,2],[0,3,3],[1,3,4],[2,3,5],[2,2,6],[1,2,7],[0,2,8]], f"t1 = {t1}"
 
   t2_p1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
   t2_p2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
   t2 = intersection(t2_p1, t2_p2)
 
-  assert t2 == 159, f"t2 = {t2}"
+  assert t2 == 610, f"t2 = {t2}"
 
   t3_p1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(",")
   t3_p2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(",")
   t3 = intersection(t3_p1, t3_p2)
 
-  assert t3 == 135, f"t3 = {t3}"
+  assert t3 == 410, f"t3 = {t3}"
 
   print("ok")
